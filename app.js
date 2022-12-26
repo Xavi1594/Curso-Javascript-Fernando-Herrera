a41d4b9d4f61d1c162dd47d8d3be536ad747768b
Xavi1594 <xavi9415@hotmail.com>
2022-12-26 18:53:14 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let arregloCosas = [
+    true,
+    123,
+    "Xavi",
+    12, 
+    function(){},
+    ()=>{},
+    {a: 1},
+    ["X", "Megaman", "Zero", "Dr. Light" ]
+]
+let resultado = arregloCosas [7];
+let cositas = resultado[3]
+console.log(cositas);
---
+++ juegos.js
@@ -0,0 +1,18 @@
+
+let personaje = {
+    nombre: "Stark",
+    codeName: "IronMan",
+    Vivo: false,
+    edad: 40 ,
+    coords: {
+        lat:46474,
+        lng: -754
+    },
+    trajes: ["Mark i", "Mark v", "Hulkbuster"],
+direccion: {
+    zip: 'sadaad, 45645454544',
+    ubicacion: 'MALIBYU, CALIFORNIA',
+
+}
+};
+console.log('trajes', personaje.trajes.length-1);
